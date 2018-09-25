6663bba1c5aee92ba61f5ee4ef55af15db6952c1
Granafmini <41004653+Granafmini@users.noreply.github.com>
2018-09-25 15:07:58 +0530

    Create polyndrome.py

---
+++ polyndrome.py
@@ -0,0 +1,13 @@
+print("chek whether a number is polyndrome or not")
+N=int(input("number="))
+if(N<=1000):
+ t=N
+ rev=0
+ while N>0:
+    re=N%10
+    rev=rev*10+re
+    N=N//10
+ if(t==rev):
+  print("palindrome")
+ else:
+    print("not a palindrome")
