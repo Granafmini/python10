9e81297b6a8b595496cef2941368cafd19500ff8
Granafmini <41004653+Granafmini@users.noreply.github.com>
2018-09-26 15:31:33 +0530

    Create minimum.py

---
+++ minimum.py
@@ -0,0 +1,11 @@
+print("print minimum element")
+from array import*
+n=int(input("N="))
+for y in range(1,n-1);
+   my=array('i',[y])
+m=my[0]
+for x in my:
+   if(x<=my[0]):
+       m=x
+print("mini",m)
+
